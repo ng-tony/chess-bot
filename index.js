400bafa45aa280ee763cffa77f91bc19ef00a236
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-06-26 17:32:44 -0400

    add message processesing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const token = process.env.PAGE_ACCESS_TOKEN 
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -20,11 +21,23 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'leel') {
+	if (req.query['hub.mode'] && req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
 		res.status(200).send(req.query['hub.challenge'])
 	}else{
     	res.send('Error, wrong token')
     }
+})
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
 })
 
 // Spin up the server
