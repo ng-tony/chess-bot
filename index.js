bfa2eb857f308e5f74f861e6a4114b7e9ec845c5
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-06-26 18:08:58 -0400

    cleaning it up a bit

--- index.js
+++ index.js
@@ -28,6 +28,24 @@ app.get('/webhook/', function (req, res) {
     }
 })
 
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
+
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
@@ -47,20 +65,4 @@ function sendTextMessage(sender, text) {
     })
 }
 
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-	    let event = req.body.entry[0].messaging[i]
-	    let sender = event.sender.id
-	    if (event.message && event.message.text) {
-		    let text = event.message.text
-		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-	    }
-    }
-    res.sendStatus(200)
-})
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
