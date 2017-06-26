4f1c1eb7b967f5c3c6e3d36d99ab1486f192ea31
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-06-26 17:33:57 -0400

    forgot the function, woops

--- index.js
+++ index.js
@@ -27,6 +27,26 @@ app.get('/webhook/', function (req, res) {
     	res.send('Error, wrong token')
     }
 })
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
+
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
