3e735a6cf6f37d5167c868d0ec501347a62d4826
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-06-27 16:39:58 -0400

    more style

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const token = process.env.PAGE_ACCESS_TOKEN
 
 app.set('port', (process.env.PORT || 5000))
 
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
+
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -41,11 +46,6 @@ app.post('/webhook/', function(req, res) {
 		}
 	}
 	res.sendStatus(200)
-})
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
 })
 
 function sendTextMessage(sender, text) {
