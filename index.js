8aa9ce637a6431297a86695774bfb4b75e4a5ad4
ng-tony <tonyn.20500@gmail.com>
2017-07-02 00:19:57 -0400

    Test

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function logMessage(sender, text, time){
 function repeatLastMessages(sender){
 	MongoClient.connect(process.env.MONGODB_URI , function(err, db) {
 		assert.equal(null, err);
-		var cursor = db.collection("datamine").find().limit(3).sort({timestamp:1});
+		var cursor = db.collection("datamine").find().sort({timestamp:1}).limit(3);
 		
 		cursor.toArray(function(err, results) {
 			if (err) throw err;
