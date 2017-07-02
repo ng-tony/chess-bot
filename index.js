8cf27fad56f5a353920b80fbd365fd6a58086e6b
ng-tony <tonyn.20500@gmail.com>
2017-07-01 23:40:25 -0400

    Test

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/webhook/', function(req, res) {
 app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
 	console.log("REQUEST");
-	console.log(req);
+	console.log(req.body.entry);
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
 		let sender = event.sender.id
