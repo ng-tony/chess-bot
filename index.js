82b01252e0f56c141dcb083fa264baa5e3e8ae35
ng-tony <tonyn.20500@gmail.com>
2017-07-01 23:04:01 -0400

    Added testing code for mongodb

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var MongoClient = require('mongodb').MongoClient, assert = require('assert')
 const app = express()
 const token = process.env.PAGE_ACCESS_TOKEN
 
+MongoClient.connect(process.env.MONGODB_URI , function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
+
 app.set('port', (process.env.PORT || 5000))
 
 // Spin up the server
