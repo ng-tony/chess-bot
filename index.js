9cd9e053a9bf9c5cb49f85b8cceafa8db7ae7f42
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-07-03 17:12:46 -0400

    testing chess.js again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 var MongoClient = require('mongodb').MongoClient, assert = require('assert')
-var chess = require('./chess');
+// var chess = require('./chess')
 const app = express()
 const token = process.env.PAGE_ACCESS_TOKEN
 const mongoURI = process.env.MONGODB_URI;
