f1e3e9515fbe5fb6066bab795e069971e29cbda9
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-07-03 18:18:39 -0400

    testing chess.js again

--- index.js
+++ index.js
@@ -48,14 +48,14 @@ app.get('/webhook/', function(req, res) {
 app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
-		let event = req.body.entry[0].messaging[i]
-		let sender = event.sender.id
+		let event = req.body.entry[0].messaging[i];
+		let sender = event.sender.id;
 		let timestamp = event.timestamp;
  		if (event.message && event.message.text) {
-			let text = event.message.text
-			messageHandler(sender, text)
-			logMessage(sender, text, timestamp);
-			repeatLastMessages(sender);
+			let text = event.message.text;
+			messageHandler(sender, text);
+			// logMessage(sender, text, timestamp);
+			// repeatLastMessages(sender);
 		}
 	}
 	res.sendStatus(200)
