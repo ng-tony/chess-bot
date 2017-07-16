0d16af6aa776a29c4dbc331b32e83a20d1ed9dd8
ng-tony <tonyn.20500@gmail.com>
2017-07-16 18:13:32 -0400

    Fixed Danny's code

--- gameCode.js
+++ gameCode.js
@@ -45,5 +45,6 @@ module.exports.genCode = function(dict, mongoURI){
 	var dictSize = getDictSize(dict);
 	var currCode = getCurrCode(mongoURI);
 	var newCode = makeNewCode(currCode, dictSize);
+	console.log(currCode);
 	return currCode;
 }
