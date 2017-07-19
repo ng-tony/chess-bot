8181cebae582de70f631f0c6312c533488beae83
Danny Huang <dannyhuanggg@gmail.com>
2017-07-19 20:37:34 +0000

    bs

--- gameCode.js
+++ gameCode.js
@@ -5,16 +5,20 @@ var fs = require('fs'),
 	assert = require('assert');
 
 function getDictSize(dict){
-	var dictSize = 0;
-	var rl = readline.createInterface({
-		input: fs.createReadStream(dict),
-		output: process.stdout,
-		terminal: false
+	return new Promise(function(resolve, reject){
+		var dictSize = 0;
+		var rl = readline.createInterface({
+			input: fs.createReadStream(dict),
+			output: process.stdout,
+			terminal: false
+		});
+		rl.on('line', function(line){
+			dictSize += 1;
+		});
+		rl.on('close', function(){
+			resolve(dictSize);
+		})
 	});
-	rl.on('line',function(line){
-		dictSize += 1;
-	});
-	return dictSize;
 }
 
 function getCurrCode(mongoURI){
@@ -39,16 +43,40 @@ function getCurrCode(mongoURI){
 	});
 }
 
-function makeNewCode(currCode, dictSize){
-
+/*limitation, if codeCounter exceeds [n,n,n,n], where n
+is dictSize, counter does not change*/
+function makeNewCode(codeCounter, dictSize){
+	/*
+	codeCounter is an array counting current permutation of game code
+	returned by the currcode promise
+	
+	
+	*/
+	
+	/*!!! still need to return code Counter after change, idk how to do that
+	for promises*/
+	return new Promise(function (resolve, reject){
+		var i;
+		for(i = (codeCounter.length - 1); i > 0; i--){
+			if((codeCounter[i] + 1) > (dictSize - 1) && (i - 1 >= 0)){
+				codeCounter[i] = 0;
+				codeCounter[i - 1] += 1;
+			}
+		}
+	});
 }
 
 /*needs the mongoURI and the local dictionary JSON file path
 chose to take in mongoURI rather than get from process because this is module*/
-module.exports.genCode = function(dict, mongoURI){ 
-	var dictSize = getDictSize(dict);
-	var currCode = getCurrCode(mongoURI);
-	console.log("get Promise: " + currCode);
-	var newCode = makeNewCode(currCode, dictSize);
-	return currCode;
+module.exports.genCode = function(dict, mongoURI){
+	return new Promise(function(resolve, reject){
+		var makeCode;
+		var getCode;
+		getDictSize(dict).then(function(dictSize){
+			makeCode = makeNewCode.bind(null, dictSize);
+			getCode = getCurrCode.bind(mongoURI);
+		})
+		.then(getCurrCode)
+		.then(makeCode);
+	});
 }
