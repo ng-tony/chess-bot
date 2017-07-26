a11e5ae82abea177d53ba8ca4f1f24d0dc4fa790
yhuang133 <yhuang133@mail.uoguelph.ca>
2017-07-26 02:03:00 +0000

    cleaned up file, removed unused functions

--- gameCode.js
+++ gameCode.js
@@ -8,103 +8,12 @@ var fs = require('fs'),
 	assert = require('assert'),
 	codeCounter = "codeCounter",
 	dict = "dict"; 
+	
 function showTest(){
 	console.log("showTest");
 	console.log("Dict: " + dict);
 	console.log("codeCounter: " + codeCounter)
 }
-function init(){
-	MongoClient.connect(mongoURI , function(err, db){
-		if(err){
-			console.log("GET CURRCODE: OPENING", err);
-		} else {
-			var collection = db.collection('codeCounter');
-			collection.find().toArray(function(err, res) {
-				if(err){
-					console.log("GET CURRCODE: READING", err);
-				} else{
-					console.log(res[0]);
-					codeCounter = JSON.parse(JSON.stringify(res[0].codeCounter));
-					showTest();
-					//
-				}
-			})
-		}
-	});
-
-	fs.readFile('dict.json', 'utf8', function (err, data) {
-		if(err) throw err;
-		console.log(JSON.parse(data));
-		dict = JSON.parse(data);
-		showTest();
-	});
-}
-init();
-
-//IDK Why the latter dont work but im sure this part does
-/*module.exports.codeCounter = {};
-module.exports.dict = {};
-module.exports.initf = false;*/
-/*var codeCounter = [];
-var dict = []; 
-var initf = false;
-
-var init = function() {
-	//get codeCounter
-	console.log("initing");//
-	console.log(init);
-	if(this.initf) {
-		return; //
-	}
-	initf = true;
-	MongoClient.connect(mongoURI , function(err, db){
-		if(err){
-			console.log("GET CURRCODE: OPENING", err);
-		} else {
-			var collection = db.collection('codeCounter');
-			collection.find().toArray(function(err, res) {
-				if(err){
-					console.log("GET CURRCODE: READING", err);
-				} else{
-					console.log(res[0]);
-					codeCounter = (res[0].codeCounter);
-					//
-				}
-			})
-		}
-	});
-
-	//getDict
-	fs.readFile('dict.json', 'utf8', function (err, data) {
-		if(err) throw err;
-		console.log(JSON.parse(data));
-		dict = JSON.parse(data);
-	})
-}
-*/
-
-/*
-function getCurrCode(){
-	return new Promise(function (resolve, reject){
-		MongoClient.connect(mongoURI , function(err, db){
-			if(err){
-				console.log("GET CURRCODE: OPENING", err);
-				reject(err);
-			} else {
-					var collection = db.collection('codeCounter');
-					collection.find().toArray(function(err, res) {
-					if(err){
-						console.log("GET CURRCODE: READING", err);
-					} else{
-						console.log(res[0]);
-						resolve(res[0].codeCounter);
-						//
-					}
-					})
-				}
-			});
-	});
-}*/
 
 /*limitation, if codeCounter exceeds [n,n,n,n], where n
 is dictSize, counter does not change*/
@@ -134,7 +43,6 @@ var makeNewCode = function(){
 		MongoClient.connect(mongoURI , function(err, db){
 			if(err){
 				console.log("WRITING CURRCODE TO DB: ", err);
-				reject(err);
 			} else {
 					var collection = db.collection('codeCounter');
 					collection.updateOne({}, {"codeCounter" : codeCounter}, function(err, res) {
@@ -149,7 +57,7 @@ var makeNewCode = function(){
 		return codeCounter;
 }
 
-function genCode(){
+module.exports.genCode = function genCode(){
 	console.log("genCode");
 	console.log("fs: " + fs);
 	console.log("codeCounter: " + codeCounter);
@@ -158,41 +66,4 @@ function genCode(){
 		//var makeCode = makeNewCode.bind(codeCounter, dict.length);
 		resolve(makeNewCode());
 	});
-}
-
-/*needs the mongoURI and the local dictionary JSON file path
-chose to take in mongoURI rather than get from process because this is module*/
-/*exports.genCode = genCode;*/
-/*var Game = function (){
-
-	MongoClient.connect(mongoURI , function(err, db){
-		if(err){
-			console.log("GET CURRCODE: OPENING", err);
-		} else {
-			var collection = db.collection('codeCounter');
-			collection.find().toArray(function(err, res) {
-				if(err){
-					console.log("GET CURRCODE: READING", err);
-				} else{
-					console.log(res[0]);
-					codeCounter = (res[0].codeCounter);
-					//
-				}
-			})
-		}
-	});
-
-	//getDict
-	fs.readFile('dict.json', 'utf8', function (err, data) {
-		if(err) throw err;
-		console.log(JSON.parse(data));
-		dict = JSON.parse(data);
-	})
-
-	this.dict = dict;
-	this.codeCounter = codeCounter;
 };
-
-Game.prototype.makeNewCode = makeNewCode;
-Game.prototype.genCode = genCode;*/
-module.exports.genCode = genCode;
