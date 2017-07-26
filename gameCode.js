a2142731687d295df0a3eb075aef41acb11853d3
Tony Ng <tonyn.20500@gmail.com>
2017-07-26 16:56:22 -0400

    gonna work this time

--- gameCode.js
+++ gameCode.js
@@ -53,6 +53,9 @@ var makeNewCode = function(){
 		console.log(codeCounter);
 		codeCounter[curr]++;
 		while (!isFinished && curr > 0) {
+			console.log((Number(codeCounter[curr]) > (dict.length - 1));
+			console.log(Number(codeCounter[curr]));
+			console.log(dict.length - 1);
 			if (Number(codeCounter[curr]) > (dict.length - 1)){
 				codeCounter[curr] = 0;
 				curr--;
