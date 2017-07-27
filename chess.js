52ca5bd07b233574c4e19dc422ee32de24b5ec8d
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-07-27 05:25:21 +0000

    starting to work on gamecode

--- chess.js
+++ chess.js
@@ -6,9 +6,9 @@ converts it to value that can be used for accessing
 the board array
 **/
 module.exports.coordToArrPos = function (val){
-	const convert = { "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, 
-		"f": 5, "g": 6, "h": 7, 8: 0, 7: 1, 6: 2, 
-		5: 3, 4: 4, 3: 5, 2: 6, 1: 7};
+	const convert = { 
+		"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, 
+		"8": 0, "7": 1, "6": 2, "5": 3, "4": 4, "3": 5, "2": 6, "1": 7};
 
 	return convert[val];
 }
@@ -32,4 +32,18 @@ module.exports.initBoard = function (){
 }
 
 module.exports.isValidMove = function (color, piece, startX, startY, destX, destY, board){
+	switch(piece){
+		case "P":
+			break;
+		case "R":
+			break;
+		case "N":
+			break;
+		case "B":
+			break;
+		case "Q":
+			break;
+		case "K":
+			break;
+	}
 }
