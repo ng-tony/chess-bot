bb2d67d93102a9edc03e58372f4d88e85aad1fee
ng-tony <tonyn.20500@gmail.com>
2017-08-14 00:43:02 -0400

    idk

--- imageTest.js
+++ imageTest.js
@@ -2,23 +2,30 @@
 var Jimp = require('jimp');
  
 module.exports.createTestImage = function(){
+	console.log("Entering create Test Image");
 	var image = new Jimp(256, 256, function (err, image){
 		//idk if i need this call back;
 	});
+	console.log("Entering create Test Image");
 	console.log(image.getExtension());
-	image.write("./output.png", function(err){
+	image.write("./output.png").then(function () {
+		console.log("Made image");
+	}).catch(function () {
+		console.log("Error creating image");
 		console.log(err);
-	})
+	});
+	console.log("Exiting create Test Image");
 }
 
 module.exports.doesImageExists = function(){
-	Jimp.read("./output.png", function (err, image){
-		if(err){
-			console.log("Image not tehre");
-			return;
-		}
+	console.log("Entering does image exists");
+	Jimp.read("./output.png").then(function (image){
 		console.log(image.getExtension());
 		console.log("Image is here?!?!?!?");
 		
+	}).catch(function (err) {
+		console.log("Image not tehre");
+			return;
 	})
+	console.log("Exiting does image exists");	
 }
