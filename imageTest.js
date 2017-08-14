31a8ff04d29852a8bcd0007999b28844a69e0685
ng-tony <tonyn.20500@gmail.com>
2017-08-14 00:01:00 -0400

    ok maybe needed to  be apromise or maybe because i wrote the filename wrong

--- imageTest.js
+++ imageTest.js
@@ -5,7 +5,7 @@ module.exports.createTestImage = function(){
 	var image = new Jimp(256, 256, function (err, image){
 		//idk if i need this call back;
 	});
-	image.write("./output.png,", function(){
+	image.write("./output.png", function(){
 		console.log("sucess wriitng");
 	})
 }
