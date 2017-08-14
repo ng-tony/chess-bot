d818eed0c3538b0055ae3433af33fefc241212ca
ng-tony <tonyn.20500@gmail.com>
2017-08-13 23:52:05 -0400

    Heellllo

--- imageTest.js
+++ imageTest.js
@@ -1,29 +1,11 @@
 // Require library 
-var gd = require('node-gd');
+var Jimp = require('jimp');
  
 module.exports.createTestImage = function(){
-// Create blank new image in memory 
-var img = gd.createSync(200, 80);
- 
-// Set background color 
-img.colorAllocate(0, 255, 0);
- 
-// Set text color 
-var txtColor = img.colorAllocate(255, 0, 255);
- 
-// Set full path to font file 
-var fontPath = '/full/path/to/font.ttf';
- 
-// Render string in image 
-img.stringFT(txtColor, fontPath, 24, 0, 10, 60, 'Hello world!');
- 
-// Write image buffer to disk 
-img.savePng('output.png', 1, function(err) {
-  if(err) {
-    throw err;
-  }
-});
- 
-// Destroy image to clean memory 
-img.destroy();
+	var image = new Jimp(256, 256, function (err, image){
+		//idk if i need this call back;
+	});
+	image.write("./output.png,", function(){
+		console.log("sucess wriitng");
+	})
 }
