00fcd3cc50affbeef137d4fedb61ba9bc5bdb57a
ng-tony <tonyn.20500@gmail.com>
2017-08-15 22:49:49 -0400

    C if i can do it without writing to disc

--- imageTest.js
+++ imageTest.js
@@ -1,6 +1,17 @@
 // Require library 
 var Jimp = require('jimp');
- 
+var baseImage = 
+(function(){
+	return new Promise(function (resolve, reject){
+		Jimp.read("base_board.png").then(function(image){
+			resolve(image);
+		}).catch(function(err){
+			console.log("ERROR LOADING BASE IMAGE: " + err);
+			reject(err);
+		})
+	})
+})();
+
 module.exports.createTestImage = function(){
 	return new Promise(function (resolve, reject){
 		console.log("Entering create Test Image");
@@ -19,13 +30,11 @@ module.exports.createImage = function(board){
 	//do some shit with board
 	return new Promise(function (resolve, reject){
 		console.log("Entering create Test Image");
-		var image = new Jimp(256, 256, function (err, image){
-			//idk if i need this call back;
-			image.getBuffer(Jimp.MIME_PNG, function(err, thing){
-				console.log("err: "+err);
-				console.log(thing);
-				resolve(thing);
-			})
+		var image = image.clone();
+		return image.getBuffer(Jimp.MIME_PNG, function(err, thing){
+			console.log("err: " + err);
+			console.log(thing);
+			resolve(thing);
 		});
 	})
 }
