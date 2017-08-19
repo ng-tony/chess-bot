f883d4cba7dd157f0138591fdb1601a9122ec348
ng-tony <tonyn.20500@gmail.com>
2017-08-19 18:05:54 -0400

    But why anon fucnt not work

--- imageGenerator.js
+++ imageGenerator.js
@@ -10,7 +10,7 @@ var coords = {
 	"wR": {x: 440, y: 0}, "bR":{x: 440, y: 110},
 	"wP": {x: 550, y: 0}, "bP":{x: 550, y: 110}
 }
-(function(){
+
 	var baseImageP = Jimp.read("./assets/base_board.png");
 	var chessPieceP = Jimp.read("./assets/chess_pieces.png");
 	Promise.all([baseImageP, chessPieceP]).then(function(values){
@@ -19,7 +19,7 @@ var coords = {
 	}).catch(function(err){
 		console.log("ERROR LOADING PICTURES: " + err);
 	});
-})();
+
 module.exports.createTestImage = function(){
 	return new Promise(function (resolve, reject){
 		console.log("Entering create Test Image");
