d4c4ec255d60b1d916c71e2bbf61cfc60ebcca6b
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-08-25 21:47:58 +0000

    forgot to pass in board

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ function getGameInfo(sender, movePhrase){
 function isValidMove(game, movePhrase, sender){
 	var board = game.board;
 	var color = (game.turnNum % 2) ? "white" : "black";
-	var moveInfo = chess.getMoveInfo(movePhrase);
+	var moveInfo = chess.getMoveInfo(movePhrase, board);
 	if (sender !== game[color]){
 		sendTextMessage(sender, "It's not your turn");
 		return false; 
