c3dc4c1d4d6c3299c82ef2e3108c37e83c264c26
yhuang133 <yhuang13@mail.uoguelph.ca>
2017-08-25 23:11:31 +0000

    cannot read property 4 debugging

--- index.js
+++ index.js
@@ -144,6 +144,7 @@ function messageHandler(sender, text){
 			}
 			getGame(sender).then((game) => { 
 				if(isValidMove(game, movePhrase, sender)){
+					console.log("done that?");
 					updateGame(game, movePhrase, sender).then((updatedGame) => {
 						messagePlayers(updatedGame, movePhrase);
 					}).catch(error => {
